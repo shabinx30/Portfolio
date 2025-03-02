f0108137f719b3659539a65f82f1a5a24344f3fd
shabinx30 <shabeensharih@gmail.com>
2025-03-02 21:08:50 +0530

    Updated .gitignore and removed unwanted files

---
+++ frontend/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page anee</div>
+  )
+}
+
+export default page
---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+    return (
+        <div>
+            <h1>PORTFOLIO 2.5</h1>
+        </div>
+    );
+}
