d139897925b87575a2a996a01b53f2ec171dae3b
shabinx30 <shabeensharih@gmail.com>
2025-05-06 14:57:03 +0530

    refactor: comment out unused ProfileData interface and profile object in Home component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,12 +5,12 @@ import Hero from "../components/Hero";
 
 
 
-interface ProfileData {
-    name: string;
-    intro: string;
-    skillsDescription: string;
-    img: string;
-}
+// interface ProfileData {
+//     name: string;
+//     intro: string;
+//     skillsDescription: string;
+//     img: string;
+// }
 
 export async function generateStaticParams() {
     return [];
@@ -26,12 +26,12 @@ export default async function Home() {
     //     // { link: '#', text: 'Sequoia', image: 'https://picsum.photos/600/400?random=4' }
     // ];
 
-    const profile: ProfileData = {
-        name: "Shabeen Sharih",
-        intro: "I'm a highly passionate web developer, constantly challenging my problem-solving skills and striving for consistency. Every day, I work diligently to enhance my abilities and grow in my craft.",
-        skillsDescription: "As a Full Stack Developer, I specialize in web development, combining frontend development & backend development to build seamless digital experiences. My expertise includes modern frameworks like React.js & Next.js, alongside robust server-side technologies such as Node.js & TypeScript. With strong skills in MongoDB, PostgreSQL, & RESTful APIs, I efficiently manage databases & ensure data integrity. I focus on responsive design, web performance optimization, & UI/UX design to deliver user-friendly interfaces. Leveraging tools like Git for version control, I thrive in Agile development environments & am experienced in cloud deployment solutions. My goal is to build scalable, high-performance web applications that enhance user engagement & drive business growth.",
-        img: "https://instagram.fcok10-4.fna.fbcdn.net/v/t51.2885-19/476501464_1523240058345855_3565333406899966342_n.jpg?_nc_ht=instagram.fcok10-4.fna.fbcdn.net&_nc_cat=103&_nc_oc=Q6cZ2AFeCYm11zBCiIvc8RQQ7g8rjat-Hg43kkkkkwySQw51EW8GmSVV9uSRTuohHRb8jRZXfGz8qY4J34vgW76V-eT9&_nc_ohc=RUriHpRQhYsQ7kNvgFuVKph&_nc_gid=752596427b0a4110bb2db4995adcc603&edm=ALGbJPMBAAAA&ccb=7-5&oh=00_AYFB6aPeQ4gRFk4AQtXOU8x_3p2yRVnSrlcsYGcB7Ojogw&oe=67D3705A&_nc_sid=7d3ac5"
-    };
+    // const profile: ProfileData = {
+    //     name: "Shabeen Sharih",
+    //     intro: "I'm a highly passionate web developer, constantly challenging my problem-solving skills and striving for consistency. Every day, I work diligently to enhance my abilities and grow in my craft.",
+    //     skillsDescription: "As a Full Stack Developer, I specialize in web development, combining frontend development & backend development to build seamless digital experiences. My expertise includes modern frameworks like React.js & Next.js, alongside robust server-side technologies such as Node.js & TypeScript. With strong skills in MongoDB, PostgreSQL, & RESTful APIs, I efficiently manage databases & ensure data integrity. I focus on responsive design, web performance optimization, & UI/UX design to deliver user-friendly interfaces. Leveraging tools like Git for version control, I thrive in Agile development environments & am experienced in cloud deployment solutions. My goal is to build scalable, high-performance web applications that enhance user engagement & drive business growth.",
+    //     img: "https://instagram.fcok10-4.fna.fbcdn.net/v/t51.2885-19/476501464_1523240058345855_3565333406899966342_n.jpg?_nc_ht=instagram.fcok10-4.fna.fbcdn.net&_nc_cat=103&_nc_oc=Q6cZ2AFeCYm11zBCiIvc8RQQ7g8rjat-Hg43kkkkkwySQw51EW8GmSVV9uSRTuohHRb8jRZXfGz8qY4J34vgW76V-eT9&_nc_ohc=RUriHpRQhYsQ7kNvgFuVKph&_nc_gid=752596427b0a4110bb2db4995adcc603&edm=ALGbJPMBAAAA&ccb=7-5&oh=00_AYFB6aPeQ4gRFk4AQtXOU8x_3p2yRVnSrlcsYGcB7Ojogw&oe=67D3705A&_nc_sid=7d3ac5"
+    // };
 
     return (
         <>
