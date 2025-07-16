dec4844bcd1b2a9fe19d8b0f69c1a79718d6c9d3
shabinx30 <shabeensharih@gmail.com>
2025-07-16 17:48:40 +0530

    Add: skill icon

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -5,7 +5,7 @@ import SkillItem from "./skill/SkillItem";
 
 const Skill = () => {
     const skills = {
-        frontend: ["html", "css", "react", "next", "tailwind", "bootstrap"],
+        frontend: ["html", "css", "react", "next", "tailwind", "bootstrap", "materialui"],
         backend: ["nodejs", "express", "mongodb", "postgres", "firebase"],
         toolsLaguages: ["git", "github", "figma" , {name: "javascript", icon: "js"}, {name: "typescript", icon: "ts"}],
         cloudDeployment: ["vercel", "aws"],
