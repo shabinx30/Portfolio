a84beb10b5fa3fcb2ba746c3b6fe9ea84112c5e4
shabinx30 <shabeensharih@gmail.com>
2025-07-21 13:45:03 +0530

    style(Hero): gsap animations added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { motion, useInView } from "framer-motion";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 import Link from "next/link";
 import { useAppContext } from "@/context/AppContext";
+import gsap from "gsap";
 
 const Hero = () => {
     const { homeRef } = useAppContext();
+    const greRef = useRef(null);
+    const nameRef = useRef(null);
+    const disRef = useRef(null);
     const skillsRef = useRef(null);
     const isSkillsInView = useInView(skillsRef, {
         amount: 0.5,
@@ -21,6 +25,35 @@ const Hero = () => {
         visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
     };
 
+    useEffect(() => {
+        gsap.from(greRef.current, {
+            yPercent: 130,
+            opacity: 0,
+            duration: 1.2,
+            ease: "power4.out",
+            stagger: 0.1,
+            delay: 0.1,
+        });
+
+        gsap.from(nameRef.current, {
+            yPercent: 130,
+            opacity: 0,
+            duration: 1.2,
+            ease: "power4.out",
+            stagger: 0.1,
+            delay: 0.15,
+        });
+
+        gsap.from(disRef.current, {
+            yPercent: 130,
+            opacity: 0,
+            duration: 1.2,
+            ease: "power4.out",
+            stagger: 0.1,
+            delay: 0.2,
+        });
+    }, []);
+
     return (
         <section
             ref={homeRef}
@@ -28,21 +61,53 @@ const Hero = () => {
         >
             <div className="absolute flex justify-center pt-[14%] dark:bg-black/30 z-40 backdrop-blur-[50px] h-[114dvh] w-[100vw]">
                 <div className="text-center pt-[30%] md:p-0 w-[90%] lg:w-[47%]">
-                    <h1 className="text-2xl lg:text-4xl HN-medium mb-2 dark:text-transparent bg-clip-text text-black dark:bg-gradient-to-b from-[#C2FF78] to-[#C2FF78]/30">
-                        Hello,
-                    </h1>
-                    <h1 className="text-3xl lg:text-[4em] dark:text-transparent HN-medium font-extralight bg-clip-text text-black dark:bg-gradient-to-r to-white/30 via-white from-white/30">
-                        I&apos;m Shabeen Sharih
-                    </h1>
-                    <p className="text-[#7b7b7b] dark:text-[#AEAEAE] text-[0.65em] font-semibold lg:text-[0.825em] mt-8">
-                        A Passionate{" "}
-                        <span className="dark:text-[#C2FF78] text-black font-bold">
-                            Full Stack Web Developer
-                        </span>
-                        , specializing in the MERN stack, with expertise in
-                        MongoDB, Express.js, React, and Node.js.
-                    </p>
-                    <div className="flex gap-7 justify-center mt-6 text-black/60 dark:text-white/50 hover:text-black dark:hover:text-white duration-200">
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        className="overflow-hidden"
+                    >
+                        <h1
+                            ref={greRef}
+                            className="text-2xl lg:text-4xl HN-medium mb-2 dark:text-transparent bg-clip-text text-black dark:bg-gradient-to-b from-[#C2FF78] to-[#C2FF78]/30"
+                        >
+                            Hello,
+                        </h1>
+                    </motion.div>
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        className="overflow-hidden"
+                    >
+                        <h1
+                            ref={nameRef}
+                            className="text-3xl lg:text-[4em] dark:text-transparent HN-medium font-extralight bg-clip-text text-black dark:bg-gradient-to-r to-white/30 via-white from-white/30"
+                        >
+                            I&apos;m Shabeen Sharih
+                        </h1>
+                    </motion.div>
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        className="overflow-hidden"
+                    >
+                        <p
+                            ref={disRef}
+                            className="text-[#7b7b7b] dark:text-[#AEAEAE] text-[0.65em] font-semibold lg:text-[0.825em] mt-8"
+                        >
+                            A Passionate{" "}
+                            <span className="dark:text-[#C2FF78] text-black font-bold">
+                                Full Stack Web Developer
+                            </span>
+                            , specializing in the MERN stack, with expertise in
+                            MongoDB, Express.js, React, and Node.js.
+                        </p>
+                    </motion.div>
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1}}
+                        transition={{delay: 0.25}}
+                        className="flex gap-7 justify-center mt-6 text-black/60 dark:text-white/50 hover:text-black dark:hover:text-white duration-200"
+                    >
                         <Link
                             href="https://www.linkedin.com/in/shabeen-sharih/"
                             target="_blank"
@@ -61,7 +126,7 @@ const Hero = () => {
                         >
                             <SiGmail size={20} />
                         </Link>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
 
